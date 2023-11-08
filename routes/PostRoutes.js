97c798844b2e1d8e1ec7d9fc2379d0e21164cd6a
WEATHER API WEBSITE <mohammedjinia@gmail.com>
2023-11-08 17:18:14 +0530

    3rd

--- routes/PostRoutes.js
+++ routes/PostRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const { request_post } = require("../controllers/request_functions")
 
 router.route("/api/v1/dalle").post(request_post).get(request_post)
-router.route("/api/get").get(request_post)
+
+
 
 module.exports = router
